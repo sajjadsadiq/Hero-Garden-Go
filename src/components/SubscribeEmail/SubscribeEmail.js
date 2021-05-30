c01059dec6125beced858913603e3b5a02d4addc
sajjad sadiq <sajjadsadiq2020@gmail.com>
2021-05-30 22:47:51 +0600

    update version 3

--- src/components/SubscribeEmail/SubscribeEmail.js
+++ src/components/SubscribeEmail/SubscribeEmail.js
@@ -10,7 +10,6 @@ const SubscribeEmail = () => {
                     <p>Commodo sociosqu venenatis cras i do dolor sagittis integer luctus sem primis eget maecenas sed urna malesuada consectetuer.</p>
                     <input type="email" name="" id="" placeholder="Enter Your Email" /> <br />
                     <div className="btn btn-primary mt-4 mb-5 p-3">Subscribe Now</div>
-                    <p>Lorem ipsum dolor sit amet.</p>
                 </div>
             </div>
         </div>
